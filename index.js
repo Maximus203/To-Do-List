e73c89b322258a7f843f1efbe8cb901a28f74687
Maximus203 <printf0cherif@gmail.com>
2022-08-17 10:56:17 +0000

    version 1

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+    We are going to create a basic to do list app in javascript with the following features:
+    
+        Add new task
+        Delete task
+        Mark task as completed
+        Edit task
+        Sort tasks by date
+        Search tasks
+
+*/
+
+
+
+
